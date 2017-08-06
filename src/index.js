c85efc96ea04f728b55d7b9ce911253ad4faf277
lisovskiy <d.lisovskiy@theomnicore.com>
2017-08-06 14:06:27 +0300

    article

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {render} from 'react-dom'
+import Article from './Article'
+import {articles} from './fixtures'
+
+const article = articles[0];
+render(<Article article = {article} />, document.getElementById('container'));
