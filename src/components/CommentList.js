6c0f700c71f4c4dfc6ac033fe10bd56b14c5acba
lisovskiy <d.lisovskiy@theomnicore.com>
2017-08-13 17:54:41 +0300

    rewrite commentsList to statless

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,39 +1,27 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Comment from './Comment'
 import toggleOpen from '../decorators/toggleOpen'
 
-class CommentList extends Component {
-    static defaultProps = {
-        comments: [
-            {
-                id: "sdaasd",
-                user: "You",
-                text: "Your comment"
-            }
-        ]
-    }
-    render() {
-        const {comments, isOpen, toggleOpen} = this.props;
-        const commensElems = comments.map((comment) => <li key = {comment.id}><Comment comment = {comment} /></li>)
-        const textBtn = isOpen ? 'hide comments' : 'show comments'
-        return (
-            <div>
-                {commensElems 
-                    ? 
-                    <button onClick = {toggleOpen}>
-                        {textBtn}
-                    </button>
-                    : 
-                    null}
-                {isOpen 
-                    ?
-                    <ul>
-                        {commensElems}
-                    </ul>
-                    :
-                    null}
-            </div>
-        )
-    }
+function CommentList({comments = [], isOpen, toggleOpen}) {
+    const commensElems = comments.map((comment) => <li key = {comment.id}><Comment comment = {comment} /></li>)
+    const textBtn = isOpen ? 'hide comments' : 'show comments'
+    return (
+        <div>
+            {commensElems 
+                ? 
+                <button onClick = {toggleOpen}>
+                    {textBtn}
+                </button>
+                : 
+                null}
+            {isOpen 
+                ?
+                <ul>
+                    {commensElems}
+                </ul>
+                :
+                null}
+        </div>
+    )
 }
 export default toggleOpen(CommentList)
