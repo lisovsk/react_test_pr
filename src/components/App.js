37db5b8dd07cc94afcc78cb3034de4c3ae1628d7
lisovskiy <d.lisovskiy@theomnicore.com>
2017-08-21 01:26:26 +0300

    add user form

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import ArticleList from './ArticleList'
 import ArticleCart from './ArticleCart'
+import UserForm from './UserForm'
 
 class App extends Component {
+
     render() {
         return (
             <div>
+                <UserForm />
                 <ArticleList articles = {this.props.articles} />
                 <ArticleCart articles = {this.props.articles} /> 
             </div>
---
+++ src/components/UserForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class componentName extends Component {
+    state = {
+        username: ''
+    }
+
+    render() {
+        return (
+            <div>
+                Name: <input type = 'text' value = {this.state.username} onChange = {this.handleUserChange} />
+            </div>
+        );
+    }
+    handleUserChange = (ev) => {
+        if (ev.target.value.length > 10) return 
+        this.setState({
+            username: ev.target.value
+        })
+    }
+}
