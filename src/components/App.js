af8c193d3a792b0770f5d976b72e9446404edb07
lisovskiy <d.lisovskiy@theomnicore.com>
2017-09-10 21:55:00 +0300

    filters

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import ArticleList from './ArticleList';
-import UserForm from './UserForm';
-import Select from 'react-select';
-import 'react-select/dist/react-select.css';
-import DayPickerInput from 'react-day-picker/DayPickerInput';
-import 'react-day-picker/lib/style.css';
-import Input from './Input';
-import Counter from './Counter';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import ArticleList from "./ArticleList";
+import UserForm from "./UserForm";
+import Input from "./Input";
+import Counter from "./Counter";
+import Filters from "./Filters/index";
+
 class App extends Component {
   state = {
     selection: null
@@ -16,22 +14,11 @@ class App extends Component {
   changeSelection = selection => this.setState({ selection });
 
   render() {
-    // const { articles } = this.props;
-    // const options = this.props.articles.map(article => ({
-    //   label: article.title,
-    //   value: article.id
-    // }));
     return (
       <div>
         <Counter />
         <UserForm />
-        <DayPickerInput placeholder="DD/MM/YYYY" format="DD/MM/YYYY" />
-        <Select
-          options={[]}
-          value={this.state.selection}
-          onChange={this.changeSelection}
-          multi
-        />
+        <Filters articles={[]} />
         <ArticleList />
       </div>
     );
